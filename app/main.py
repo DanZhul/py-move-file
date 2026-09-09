3b46d3dcd2c5ff58ae5c64ef15abde96d71fbbcc
zhul <zhuldanil@gmail.com>
2026-09-09 20:45:17 +0300

    added move_file function

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+import os
+import shutil
+
+
+def move_file(command: str) -> None:
+    if not command:
+        return
+
+    _, source, destination = command.split(" ")
+
+    destination_dir = os.path.dirname(destination)
+    if destination_dir:
+        os.makedirs(destination_dir, exist_ok=True)
+
+    shutil.copy(source, destination)
+    os.remove(source)
